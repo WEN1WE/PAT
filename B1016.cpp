828e81b3e99f9a41ce94e9ea9ff7be1d9c4763b0
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-01 20:56:46 +0800

    Create B1016.cpp

---
+++ B1016.cpp
@@ -0,0 +1,27 @@
+/*
+ * int 范围
+ * long long 范围
+ */
+
+#include <iostream>
+using namespace std;
+
+long long getNewN(long long n, long long d) {
+    long long remainder;
+    long long newN = 0;
+    while (n != 0) {
+        remainder = n % 10;
+        if (remainder == d) {
+            newN = newN * 10 + remainder;
+        }
+        n /= 10;
+    }
+    return newN;
+}
+
+int main() {
+    long long n1, n2, d1, d2;
+    cin >> n1 >> d1 >> n2 >> d2;
+    cout << getNewN(n1, d1) + getNewN(n2, d2);
+    return 0;
+}
