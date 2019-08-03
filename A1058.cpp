4fbef7fbaccb2c6b2a4d6b2ef4624c8c338bb2cd
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-03 18:30:30 +0800

    Create A1058.cpp

---
+++ A1058.cpp
@@ -0,0 +1,26 @@
+/*
+ * 1. 与B1037相同
+ */
+
+#include <iostream>
+using namespace std;
+
+long long toKnut(long long Galleon, long long Sickle, long long Knut) {
+    return Galleon * 493 + Sickle * 29 + Knut;
+}
+
+int main() {
+    long long Galleon = 0, Sickle = 0, Knut = 0;
+    long long Galleon1, Sickle1, Knut1;
+    long long Galleon2, Sickle2, Knut2;
+    scanf("%lld.%lld.%lld %lld.%lld.%lld", &Galleon1, &Sickle1, &Knut1, &Galleon2, &Sickle2, &Knut2);
+
+    Knut = toKnut(Galleon2, Sickle2, Knut2) + toKnut(Galleon1, Sickle1, Knut1);
+
+    Galleon = Knut / 493;
+    Knut = Knut % 493;
+    Sickle = Knut / 29;
+    Knut = Knut % 29;
+    cout << Galleon << "." << Sickle << "." << Knut;
+    return 0;
+}
