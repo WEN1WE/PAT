ab04b426255756bf2cf0c97934a3c7d337255281
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-03 12:08:51 +0800

    Update A1036.cpp

--- A1036.cpp
+++ A1036.cpp
@@ -1,39 +1,61 @@
+/*
+ * 1. 使用结构体来收集信息
+ */
+
 #include <iostream>
 using namespace std;
 
 struct student {
     string name;
-    char gender;
     string ID;
     int grade;
 } femaleHighest, maleLowest;
 
 int main() {
+    bool sign1 = false, sign2 = false;
     int n, grade;
     char gender;
     string name, ID;
     cin >> n;
 
-    femaleHighest.name = "Absent";
     femaleHighest.grade = 0;
-
-    maleLowest.name = "Absent";
     maleLowest.grade = 100;
 
     for (int i = 0; i < n; i++) {
         cin >> name >> gender >> ID >> grade;
         if (gender == 'F') {
+            sign1 = true;
             if (grade > femaleHighest.grade) {
                 femaleHighest.name = name;
+                femaleHighest.ID = ID;
                 femaleHighest.grade = grade;
             }
-        } else{
+        } else {
+            sign2 = true;
             if (grade < maleLowest.grade) {
                 maleLowest.name = name;
+                maleLowest.ID = ID;
                 maleLowest.grade = grade;
             }
         }
     }
 
-    if (maleLowest.name == "Absent" || female)
+    if (sign1) {
+        cout << femaleHighest.name << " " << femaleHighest.ID << endl;
+    } else {
+        cout << "Absent" << endl;
+    }
+
+    if (sign2) {
+        cout << maleLowest.name << " " << maleLowest.ID << endl;
+    } else {
+        cout << "Absent" << endl;
+    }
+
+    if (sign1 && sign2) {
+        cout << femaleHighest.grade - maleLowest.grade;
+    } else {
+        cout << "NA";
+    }
+    return 0;
 }
