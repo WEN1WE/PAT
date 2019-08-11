0e46d02fd0dbe0998a9a5d4724b061f9cc213d6a
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-11 19:20:59 +0800

    Create A1085.cpp

---
+++ A1085.cpp
@@ -0,0 +1,36 @@
+/*
+ * 1. 本题可使用二分查找或two points
+ * 2. upper_bound, lower_bound的使用
+ *  lower_bound returns an iterator pointing to the first element in the range [first,last) which has a value not less than ‘val’.
+ *  upper_bound returns an iterator pointing to the first element in the range [first,last) which has a value greater than ‘val’.
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, maxNumbers = 0, temp;
+    long long p;
+    scanf("%d %lld", &N, &p);
+    vector<long long>  numbers(N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%lld", &numbers[i]);
+    }
+    sort(numbers.begin(), numbers.end());
+
+    for (int i = 0; i + maxNumbers < N; i++) {
+        for (int j = maxNumbers + i; j < N; j++) {
+            if (numbers[j] <= p * numbers[i]) {
+                maxNumbers = max(j - i + 1, maxNumbers);
+            } else {
+                break;
+            }
+        }
+    }
+    cout << maxNumbers;
+    return 0;
+}
