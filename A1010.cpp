435507b4c54c591be6f352c39d6c5bf130cf34e2
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-12 07:54:58 +0800

    Update A1010.cpp

--- A1010.cpp
+++ A1010.cpp
@@ -1,39 +1,51 @@
 /*
  * 1. #include <cmath> // pow
+ * 2. binarySearch 找到第一个小于等于的值
+ * 3. 本题要考虑越界的情况
+ * 4. radix 不仅为36
  */
 
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-long long toTen(string s, int radix) {
+long long toDigit(char c) {
+    if (c >= 'a') {
+        return c - 'a' + 10;
+    } else {
+        return c - '0';
+    }
+}
+
+long long toTen(string s, long long radix) {
     long long result = 0;
-    for (char i : s) {
-        if (i >= 'a') {
-            result = result * radix + i - 'a' + 10;
-        } else {
-            result = result * radix + i - '0';
+    for (char c : s) {
+        result = result * radix + toDigit(c);
+        if (result < 0) {  // 越界
+            return -1;
         }
     }
     return result;
 }
 
-int binarySearch(string s, long long value) {
-    int left = 0, right = 35, mid;
-
+long long getLeft(string s) {
+    long long left = -1, temp;
     for (char c : s) {
-        if (c >= 'a') {
-            left = max(left, c - 'a' + 10);
-        } else {
-            left = max(left, c - '0');
+        temp = toDigit(c);
+        if (left <= toDigit(c)) {
+            left = temp + 1;
         }
     }
+    return left;
+}
 
-    long long temp;
+long long binarySearch(string s, long long value) {
+    long long left = getLeft(s), right = value + 1, mid, temp;
+
     while (left < right) {
         mid = (left + right) / 2;
         temp = toTen(s, mid);
-         if (temp >= value){
+         if (temp == -1 || temp >= value){
             right = mid;
         } else {
             left = mid + 1;
@@ -43,22 +55,16 @@ int binarySearch(string s, long long value) {
 }
 
 int main() {
-    string N1, N2, s;
-    long long value;
-    int tag, radix;
-    int temp;
+    string N1, N2, s1, s2;
+    long long value, radix, temp;
+    int tag;
     cin >> N1 >> N2 >> tag >> radix;
+    s1 = tag == 1 ? N1 : N2;
+    s2 = tag == 1 ? N2 : N1;
+    value = toTen(s1, radix);
+    temp = binarySearch(s2, value);
 
-    if (tag == 1) {
-        value = toTen(N1, radix);
-        s = N2;
-    } else {
-        value = toTen(N2, radix);
-        s = N1;
-    }
-
-    temp = binarySearch(s, value);
-    if (toTen(s, temp) == value) {
+    if (toTen(s2, temp) == value) {
         cout << temp;
     } else {
         cout << "Impossible";
