8af626ab87e56b672b09e7c661e8163ea1680076
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-12 12:04:33 +0800

    Update A1085.cpp

--- A1085.cpp
+++ A1085.cpp
@@ -3,6 +3,7 @@
  * 2. upper_bound, lower_bound的使用
  *  lower_bound returns an iterator pointing to the first element in the range [first,last) which has a value not less than ‘val’.
  *  upper_bound returns an iterator pointing to the first element in the range [first,last) which has a value greater than ‘val’.
+ * 3. 本题不能使用j--
  */
 
 
