85a587ae8d55727ccd383ca704f5a3195e0c8ef9
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-12 11:41:13 +0800

    Update A1048.cpp

--- A1048.cpp
+++ A1048.cpp
@@ -1,6 +1,7 @@
 /*
  * 1. 要注意，虽然硬币值只有500， 但hashTable 要建1000，防止越界
  * 2. 程序先一定要走几步
+ * 3. 本题也可以使用二分查找，但是扣了1分
  */
 
 #include <iostream>
@@ -24,4 +25,35 @@ int main() {
     }
     printf("No Solution");
     return 0;
+}
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    bool flag = false;
+    int N, M;
+    scanf("%d %d", &N, &M);
+    vector<int> values(N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &values[i]);
+    }
+
+    sort(values.begin(), values.end());
+
+    for (int i = 0; i < N; i++) {
+        int j = upper_bound(values.begin(), values.end(), M - values[i]) - values.begin();
+        if (i != (j - 1) && values[i] + values[j - 1] == M) {
+            flag = true;
+            cout << values[i] << " " << values[j - 1];
+            break;
+        }
+    }
+    if (!flag) {
+        cout << "No Solution";
+    }
 }
