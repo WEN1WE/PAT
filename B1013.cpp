d2537420bdb564fec9d6107fbce502d55d1e14d1
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-13 20:20:57 +0800

    Create B1013.cpp

---
+++ B1013.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int m, n, temp = 2, count = 0;
+    cin >> m >> n;
+
+    while (count != n) {
+        if(isPrime(temp)) {
+            count++;
+            if (count >= m) {
+                if ((count - m + 1) % 10 != 1) {
+                    cout << " ";
+                }
+                cout << temp;
+                if ((count - m + 1) % 10 == 0) {
+                    cout << endl;
+                }
+            }
+        }
+        temp++;
+    }
+    return 0;
+}
