924caa5eab4543468d6de730038c8a06b7d93cfd
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-14 11:55:40 +0800

    Create B1017.cpp

---
+++ B1017.cpp
@@ -0,0 +1,34 @@
+/*
+ * 1. 模拟手写除法的过程
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool flag = false;
+    int result[1000] = {0};
+    int B, remainder = 0;
+    string s;
+    cin >> s >> B;
+
+    for (int i = 0; i < s.length(); i++) {
+        int A = remainder * 10 + s[i] - '0';
+        result[i] = A / B;
+        remainder = A % B;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        if (result[i] > 0) {
+            flag = true;
+        }
+        if (flag) {
+            cout << result[i];
+        }
+    }
+    if (!flag) {
+        cout << "0 " << remainder;
+    } else {
+        cout << " " << remainder;
+    }
+}
