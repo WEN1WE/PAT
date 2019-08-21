b00a8eebd869010bca889be8adc7fbf29e0bb579
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-21 08:04:14 +0800

    Create A1004.cpp

---
+++ A1004.cpp
@@ -0,0 +1,51 @@
+/*
+ * 1. 深度优先搜索
+ */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 110;
+vector<int> graph[MAX];
+int cnt[MAX] = {0};
+int maxLevel = 0;
+
+void dfs(int root, int level) {
+    maxLevel = max(level, maxLevel);
+    
+    if (graph[root].empty()) {
+        cnt[level]++;
+        return;
+    }
+    
+    for (auto e : graph[root]) {
+        dfs(e, level + 1);
+    }
+}
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int id, k, child;
+        scanf("%d %d", &id, &k);
+        for (int j = 0; j < k; j++) {
+            scanf("%d", &child);
+            graph[id].push_back(child);
+        }
+    }
+    dfs(1, 1);
+    
+    for (int i = 1; i <= maxLevel; i++) {
+        if (i != 1) {
+            printf(" ");
+        }
+        printf("%d", cnt[i]);
+    }
+    
+    return 0;
+}
