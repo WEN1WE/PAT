1ed2a8ebca67374f8b416442e1759dbc1a7b6a59
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-22 16:27:13 +0800

    Create A1140.cpp

---
+++ A1140.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+string lookAndSay(string & s) {
+    char current = s[0];
+    int count = 0;
+    string result;
+    for (char c : s) {
+        if (current == c) {
+            count++;
+        } else {
+            result.append(1, current);
+            result.append(to_string(count));
+            current = c;
+            count = 1;
+        }
+    }
+    return result ;
+}
+
+int main() {
+    int n;
+    string s;
+    cin >> s >> n;
+    
+    for (int i = 0; i < n - 1; i++) {
+        s.append("*");
+        s = lookAndSay(s);
+    }
+    cout << s;
+    return 0;
+}
