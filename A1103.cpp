fcd86e8d2aa6f34cda9dbf0b2cf6aadd6bcd0a74
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-23 20:05:14 +0800

    Create A1103.cpp

---
+++ A1103.cpp
@@ -0,0 +1,65 @@
+/*
+ * 1. 本题的递归的写法很特别
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+vector<int> result;
+vector<int> tempResult;
+vector<int> power;
+
+int maxSum = 0;
+bool flag = false;
+
+void dfs(int start, int leftN, int leftK, int factorSum) {
+    if (leftN == 0 && leftK == 0) {
+        flag = true;
+        if (factorSum > maxSum) {
+            result = tempResult;
+            maxSum = factorSum;
+        }
+        return;
+    }
+    if (start < 1 || leftN <= 0 || leftK == 0) {
+        return;
+    }
+    tempResult.push_back(start);
+    dfs(start, leftN - power[start], leftK - 1, factorSum + start);
+    tempResult.pop_back();
+    dfs(start - 1, leftN, leftK, factorSum);
+}
+
+int main() {
+    int n, k, p;
+    scanf("%d %d %d", &n, &k, &p);
+    power.push_back(-1);
+    for (int i = 1; i <= n; i++) {
+        int temp = pow(i, p);
+        if (temp <= n) {
+            power.push_back(temp);
+        } else {
+            break;
+        }
+    }
+    
+    dfs(power.size() - 1, n, k, 0);
+    
+    if (flag) {
+        printf("%d = ", n);
+        for (int i = 0; i < result.size(); i++) {
+            if (i == 0) {
+                printf("%d^%d", result[i], p);
+            } else {
+                printf(" + %d^%d", result[i], p);
+            }
+        }
+    } else {
+        printf("Impossible");
+    }
+    printf("\n");
+    return 0;
+}
