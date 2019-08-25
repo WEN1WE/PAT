14bb2232dfe7367224b840d229c752871e7d0f33
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-25 08:33:32 +0800

    Update A1034.cpp

--- A1034.cpp
+++ A1034.cpp
@@ -1,19 +1,23 @@
 /*
  * 1. 对人员进行编号，方便图的处理
  * 2. 题目说的是打电话的次数，不是人数，所以人数更大
+ * 3. 打电话是相互的，所以使用邻接矩阵
+ * 4. int graph[MAX][MAX]; // 自动初始化为0
  */
 
 #include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 const int MAX = 2010;
 map<string, int> nameToID;
 map<int, string> idToName;
 vector<int> weight(MAX);
-int graph[MAX][MAX] = {0};
+int graph[MAX][MAX];
 int nOfPeople = 0;
+bool visited[MAX] = {false};
 
 int getID(string & name) {
     if (nameToID.find(name) == nameToID.end()) {
@@ -24,10 +28,40 @@ int getID(string & name) {
     return nameToID[name];
 }
 
+int head = -1;
+int maxWeight = 0;
+int totalWeight = 0;
+int members = 0;
+void dfs(int v) {
+    if (weight[v] > maxWeight) {
+        head = v;
+        maxWeight = weight[v];
+    }
+    totalWeight += weight[v];
+    visited[v] = true;
+    members++;
+    
+    for (int i = 0; i < nOfPeople; i++) {
+        if (graph[v][i] != 0 && !visited[i]) {
+            dfs(i);
+        }
+    }
+    
+}
+
+struct Head {
+    int id;
+    int members = 0;
+};
+
+bool cmp (Head h1, Head h2) {
+    return idToName[h1.id] < idToName[h2.id];
+}
 int main() {
     int n, k;
     cin >> n >> k;
-    
+    vector<Head> result;
+
     for (int i = 0; i < n; i++) {
         int time, id1, id2;
         string name1, name2;
@@ -39,4 +73,24 @@ int main() {
         graph[id1][id2] += time;
         graph[id2][id1] += time;
     }
+    
+    for (int i = 0; i < nOfPeople; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            if (totalWeight / 2 > k && members > 2) {
+                result.push_back({head, members});
+            }
+            head = -1;
+            maxWeight = 0;
+            totalWeight = 0;
+            members = 0;
+        }
+    }
+    
+    printf("%d\n", result.size());
+    sort(result.begin(), result.end(), cmp);
+    for (auto e : result) {
+        printf("%s %d\n", idToName[e.id].c_str(), e.members);
+    }
+    return 0;
 }
