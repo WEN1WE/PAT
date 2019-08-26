362fe739eb62461b14084c01c07ce30b1f691982
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-26 16:52:33 +0800

    Create A1124.cpp

---
+++ A1124.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main() {
+    bool flag = false;
+    int m, n, s, count = 0;
+    scanf("%d %d %d", &m, &n, &s);
+    map<string, bool> mp;
+    vector<string> result;
+    string st;
+    
+    for (int i = 1; i <= m; i++) { 
+        cin >> st;
+        if (flag) {
+            count++;
+        }
+        if (i == s || count == n) {
+            if (mp.find(st) != mp.end()) {
+                count--;
+                continue;
+            }
+            flag = true;
+            cout << st << endl;
+            mp[st] = true;
+            count = 0;
+        }
+    }
+    if (!flag) {
+        printf("Keep going...\n");
+    }
+    return 0;
+}
