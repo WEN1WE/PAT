422c2083af9315b16fd2e570f4e851ab9ce07208
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-27 11:34:25 +0800

    Update A1003.cpp

--- A1003.cpp
+++ A1003.cpp
@@ -1,6 +1,7 @@
 /*
  * 1. 像这种双向的问题，也许使用邻接矩阵会很方便
  * 2. fill 的用法
+ * 3. 数有多少条路径很有技巧
  * 
  */
 
@@ -13,20 +14,52 @@ const int MAX = 510;
 int n, m, C1, C2;
 int graph[MAX][MAX];
 int dist[MAX];
-int weight[MAX];
+int teamRecord[MAX];
 int teamCount[MAX];
 int nOfPath[MAX];
 bool visited[MAX];
 
+int findMin() {
+    int v = -1;
+    int minDist = INF;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i] && minDist > dist[i]) {
+            minDist = dist[i];
+            v = i;
+        }
+    }
+    return v;
+}
+
+void relax(int v) {
+    for (int w = 0; w < n; w++) {
+        if (visited[w] || graph[v][w] == INF) {
+            continue;
+        }
+        if (dist[v] + graph[v][w] < dist[w]) {
+            nOfPath[w] = nOfPath[v];
+            teamCount[w] = teamCount[v] + teamRecord[w];
+            dist[w] = dist[v] + graph[v][w];
+        } else if (dist[v] + graph[v][w] == dist[w]) {
+            nOfPath[w] += nOfPath[v];
+            teamCount[w] = max(teamCount[w], teamCount[v] + teamRecord[w]);
+        }
+    }
+}
+
 void Dijkstra(int s) {
     dist[s] = 0;
-    teamCount[s] = weight[s];
+    teamCount[s] = teamRecord[s];
     nOfPath[s] = 1;
-    
+
     for (int i = 0; i < n; i++) {
-        
+        int v = findMin();
+        if (v == -1) {
+            break;
+        }
+        visited[v] = true;
+        relax(v);
     }
-    
 }
 
 void init() {
@@ -35,18 +68,19 @@ void init() {
 }
 
 int main() {
+    init();
     scanf("%d %d %d %d", &n, &m, &C1, &C2);
-    
+
     for (int i = 0; i < n; i++) {
-        scanf("%d", &weight[i]);
+        scanf("%d", &teamRecord[i]);
     }
-    
+
     for (int i = 0; i < m; i++) {
         int c1, c2, w;
         scanf("%d %d %d", &c1, &c2, &w);
         graph[c1][c2] = w;
         graph[c2][c1] = w;
     }
-    
-    
+    Dijkstra(C1);
+    cout << nOfPath[C2] << " " << teamCount[C2] << endl;
 }
