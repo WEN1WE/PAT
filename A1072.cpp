f0661eaac320736da055296434fd3aa8d36d9a5a
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-08-27 16:10:54 +0800

    Create A1072.cpp

---
+++ A1072.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 1020;
+int graph[MAX][MAX];
+int dist[MAX];
+int visited[MAX];
+
+
+
+
+int main() {
+    int N, M, K, D;
+    scanf("%d %d %d %d", &N, &M, &K, &D);
+    
+    for (int i = 0; i < K; i++) {
+        
+    }
+    
+    
+}
