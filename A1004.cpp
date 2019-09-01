5c34603d2fbeff00c7596b033c455987293e1c29
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-01 16:01:19 +0800

    Update A1004.cpp

--- A1004.cpp
+++ A1004.cpp
@@ -49,3 +49,47 @@ int main() {
     
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+const int MAX = 110;
+vector<int> graph[MAX];
+map<int, int> mp;
+int deepest = 0;
+
+void dfs(int root, int level) {
+    if (graph[root].empty()) {
+        deepest = max(deepest, level);
+        mp[level]++;
+    }
+    for (auto e : graph[root]) {
+        dfs(e, level + 1);
+    }
+}
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    
+    for (int i = 0; i < M; i++) {
+        int id, K, child;
+        scanf("%d %d", &id, &K);
+        for (int j = 0; j < K; j++) {
+            scanf("%d", &child);
+            graph[id].push_back(child);
+        }
+    }
+    dfs(1, 0);
+    for (int i = 0; i <= deepest; i++) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", mp[i]);
+    }
+    printf("\n");
+    return 0;
+}
