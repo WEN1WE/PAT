72c6a0d02431b1c898a0e70bce2f65cc935abea1
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-01 15:13:20 +0800

    Update A1094.cpp

--- A1094.cpp
+++ A1094.cpp
@@ -1,6 +1,7 @@
 /*
  * 1. 题目有一个边界，只有一个节点
  * 2. 对于树的深度优先搜索，传递一个level参数
+ * 3. 可以使用map进行记录
  */
 
 #include <iostream>
@@ -44,4 +45,45 @@ int main() {
     cout << largest << " " << level;
     
     return 0;
+}
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+const int MAX = 110;
+vector<int> graph[MAX];
+map<int, int> mp;
+int largestP = 0;
+int largestL = 0;
+
+void dfs(int root, int level) {
+    mp[level]++;
+    if (mp[level] > largestP) {
+        largestP = mp[level];
+        largestL = level;
+    }
+    if (graph[root].empty()) {
+        return;
+    }
+    
+    for (auto e : graph[root]) {
+        dfs(e, level + 1);
+    }
+}
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < M; i++) {
+        int id, K, child;
+        scanf("%d %d", &id, &K);
+        for (int j = 0; j < K; j++) {
+            scanf("%d", &child);
+            graph[id].push_back(child);
+        }
+    }
+    dfs(1, 1);
+    cout << largestP << " " << largestL << endl;
 }
