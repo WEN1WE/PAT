31af65b21db0df554e74a08573aede42c905d8f0
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-01 17:47:21 +0800

    Update A1043.cpp

--- A1043.cpp
+++ A1043.cpp
@@ -5,6 +5,8 @@
  * 4. 本题如果采用中序和前序进行建树，有两种情况，比较麻烦（因为有重复元素，重复元素归为哪边的问题）
  * 5. 本题题意不明，没有说是插入序列，但是按照先序遍历进行插入，就是可以得到搜索二叉树，因为根的位置肯定最先插入
  * 6. 将后序和判断合为一步来做
+ * 7. 对二叉搜索树，不管是镜像还是本身，进行先序遍历，就是插入序列
+ * 8. 对于镜像问题，其实也就是左右交换问题，不用再建树
  */
 
 #include <iostream>
@@ -62,5 +64,109 @@ int main() {
     return 0;
 }
 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> preorder;
+vector<int> postorder;
+vector<int> mirrorPreorder;
+vector<int> mirrorPostorder;
+
+struct TreeNode {
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : value(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* insert(TreeNode* root, int value) {
+    if (root == NULL) {
+        return new TreeNode(value);
+    }
+    if (root->value > value) {
+        root->left = insert(root->left, value);
+    } else {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+void getPreorder(TreeNode *root) {
+    if (root == NULL) {
+        return;
+    }
+    preorder.push_back(root->value);
+    getPreorder(root->left);
+    getPreorder(root->right);
+}
+
+void getPostorder(TreeNode *root) {
+    if (root == NULL) {
+        return;
+    }
+    getPostorder(root->left);
+    getPostorder(root->right);
+    postorder.push_back(root->value);
+}
+
+void getMirrorPreorder(TreeNode * root) {
+    if (root == NULL) {
+        return;
+    }
+    mirrorPreorder.push_back(root->value);
+    getMirrorPreorder(root->right);
+    getMirrorPreorder(root->left);
+}
+
+void getMirrorPostorder(TreeNode *root) {
+    if (root == NULL) {
+        return;
+    }
+    getMirrorPostorder(root->right);
+    getMirrorPostorder(root->left);
+    mirrorPostorder.push_back(root->value);
+}
+
+void print(vector<int> & v) {
+    for (int i = 0; i < v.size(); i++) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", v[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    vector<int> sequence(N);
+
+    TreeNode* root = NULL;
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &sequence[i]);
+        root = insert(root, sequence[i]);
+    }
+    
+    getPreorder(root);
+    vector<int> temp = preorder;
+    if (sequence == preorder) {
+        printf("YES\n");
+        getPostorder(root);
+        print(postorder);
+    } else {
+        getMirrorPreorder(root);
+        if (sequence == mirrorPreorder) {
+            printf("YES\n");
+            getMirrorPostorder(root);
+            print(mirrorPostorder);
+        } else {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
 
 
