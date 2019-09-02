86ff864c2c70d5673ab7f0d997b210866fb2571e
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-02 18:24:59 +0800

    Update A1105.cpp

--- A1105.cpp
+++ A1105.cpp
@@ -3,74 +3,75 @@
  * 2. 所有的N，都要使用完
  * 3. (int)ceil(sqrt(N * 1.0)); // 向上取整 floor 向下取整
  * 4. 边界，当n为1的时候
+ * 5. 本程序最大的问题就是没法很好处理中间最好剩下的一个
  */
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
-bool cmp(int n1, int n2) {
-    return n1 > n2;
+bool cmp(int x, int y) {
+    return x > y;
 }
 
 int main() {
-    int N, row, column;
+    int N, m, n;
     scanf("%d", &N);
-    vector<int> values(N);
-    
+    vector<int> record(N);
+
     for (int i = 0; i < N; i++) {
-        scanf("%d", &values[i]);
+        scanf("%d", &record[i]);
     }
+    sort(record.begin(), record.end(), cmp);
+
     if (N == 1) {
-        printf("%d", values[0]);
+        printf("%d", record[0]);
         return 0;
     }
     
-    sort(values.begin(), values.end(), cmp);
-    
-    row = (int)ceil(sqrt(N * 1.0));
-    while (N % row != 0) {
-        row++;
+    m = int(ceil(sqrt(N)));
+    while (true) {
+        if (N % m == 0) {
+            break;
+        } else {
+            m++;
+        }
     }
-    column = N / row;
-    
-    vector<vector<int>> matrix(row);
+    n = N / m;
+    vector<vector<int>> matrix(m);
     for (auto & e : matrix) {
-        e.resize(column);
+        e.resize(n);
     }
+    int cnt = 0;
+    int left = 0, right = n - 1, up = 0, down = m - 1;
     
-    int current = 0, curRow = 0, curCol = 0;
-    int up = 0, down = row, left = 0, right = column;
-    
-    while (current < N) {
-        while (current < N && curCol < right - 1) {
-            matrix[curRow][curCol++] = values[current++];
+    while (cnt < N) {
+        for (int i = left; cnt < N && i < right; i++) {
+            matrix[up][i] = record[cnt++];
         }
-        while (current < N && curRow < down - 1) {
-            matrix[curRow++][curCol] = values[current++];
+        for (int i = up; cnt < N && i < down; i++) {
+            matrix[i][right] = record[cnt++];
         }
-        while (current < N && curCol > left) {
-            matrix[curRow][curCol--] = values[current++];
+        for (int i = right; cnt < N && i > left; i--) {
+            matrix[down][i] = record[cnt++];
         }
-        while(current < N && curRow > up) {
-            matrix[curRow--][curCol] = values[current++];
+        for (int i = down; cnt <= N - 1 && i > up; i--) {
+            matrix[i][left] = record[cnt++];
         }
-        up++;
-        down--;
+        
         left++;
         right--;
-        curCol++;
-        curRow++;
-        
-        if (current == N - 1) {
-            matrix[curRow][curCol] = values[current++];
+        up++;
+        down--;
+        if (cnt == N - 1) {
+            matrix[up][left] = record[cnt++];
         }
     }
-    for (int i = 0; i < row; i++) {
-        
-        for (int j = 0; j < column; j++) {
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
             if (j != 0) {
                 printf(" ");
             }
