09d2b0b8c9512fee3bac56cf304d10b020d32d30
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-03 16:44:49 +0800

    Update A1121.cpp

--- A1121.cpp
+++ A1121.cpp
@@ -1,45 +1,43 @@
 /*
  * 1. map的使用
+ * 2. 此题结尾不能多打印一个换行符
  */
 
 #include <iostream>
-#include <map>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    int N, M;
+    scanf("%d", &N);
     map<int, int> mp;
-    map<int, bool> record;
+    map<int, int> guests;
+    vector<int> record;
     vector<int> result;
     
-    for (int i = 0; i < n; i++) {
-        int id1, id2;
-        scanf("%d %d", &id1, &id2);
-        mp[id1] = id2;
-        mp[id2] = id1;
-    } 
-    
-    int m;
-    scanf("%d", &m);
-    for(int i = 0; i < m; i++) {
-        int id;
-        scanf("%d", &id);
-        record[id] = true;
+    for (int i = 0; i < N; i++) {
+        int c1, c2;
+        scanf("%d %d", &c1, &c2);
+        mp[c1] = c2;
+        mp[c2] = c1;
     }
     
-    for (auto e : record) {
-        if (mp.find(e.first) == mp.end()) {
-            result.push_back(e.first);
-        } else {
-            int other = mp[e.first];
-            if (record.find(other) == record.end()) {
-                result.push_back(e.first);
-            }
+    scanf("%d", &M);
+    for (int i = 0; i < M; i++) {
+        int c;
+        scanf("%d", &c);
+        record.push_back(c);
+        guests[c] = true;
+    }
+    
+    for (int i = 0; i < M; i++) {
+        if (guests.find(mp[record[i]]) == guests.end()) {
+            result.push_back(record[i]);
         }
     }
+    
     sort(result.begin(), result.end());
     printf("%d\n", result.size());
     for (int i = 0; i < result.size(); i++) {
