644b619373fcc48482a146947a22a110272d8b7e
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-04 18:50:49 +0800

    Update A1139.cpp

--- A1139.cpp
+++ A1139.cpp
@@ -4,6 +4,7 @@
  * 3. 注意 按照4位数字打印
  * 4. 注意-0000的性别问题
  * 5. 可以使用结构体保存性别
+ * 6. 使用map, 就少写一次循环
  */
 
 #include <iostream>
@@ -86,6 +87,96 @@ int main() {
             printf("%04d %04d\n", e.id1, e.id2);
         }
         result.clear();
+    }
+    return 0;
+}
+
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 10000;
+vector<bool> gender(MAX);
+map<int, map<int, bool>> graph;
+
+int toID(string & s) {
+    int id;
+    if (s[0] == '-') {
+        s = s.substr(1);
+        id = stoi(s);
+        gender[id] = true;
+    } else {
+        id = stoi(s);
+    }
+    return id;
+}
+
+struct Pair {
+    int id1;
+    int id2;
+};
+vector<Pair> result;
+
+void find(int id1, int id2) {
+    int friend1, friend2;
+    for (auto f1 : graph[id1]) {
+        friend1 = f1.first;
+        if (gender[friend1] == gender[id1] && friend1 != id2) {
+            for (auto f2 : graph[id2]) {
+                friend2 = f2.first;
+                if (gender[friend2] == gender[id2] && friend2!= id1) {
+                    if (graph[friend1].find(friend2) != graph[friend1].end()) {
+                        result.push_back({friend1, friend2});
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool cmp(Pair & p1, Pair & p2) {
+    if (p1.id1 != p2.id1) {
+        return p1.id1 < p2.id1;
+    } else {
+        return p1.id2 < p2.id2;
+    }
+}
+
+void print(vector<Pair> & v) {
+    sort(v.begin(), v.end(), cmp);
+    printf("%d\n", v.size());
+    for (auto & e : v) {
+        printf("%04d %04d\n", e.id1, e.id2);
+    }
+    v.clear();
+}
+
+int main() {
+    int N, M, K;
+    scanf("%d %d", &N, &M);
+    
+    for (int i = 0; i < M; i++) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        int id1, id2;
+        id1 = toID(s1);
+        id2 = toID(s2);
+        graph[id1][id2] = true;
+        graph[id2][id1] = true;
+    }
+    
+    scanf("%d", &K);
+    for (int i = 0; i < K; i++) {
+        int id1, id2;
+        scanf("%d %d", &id1, &id2);
+        id1 = abs(id1);
+        id2 = abs(id2);
+        find(id1, id2);
+        print(result);
     }
     return 0;
 }
