f4a71fc18147e52c8e07a742cf9db6ab83e2a984
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-04 15:24:25 +0800

    Update A1151.cpp

--- A1151.cpp
+++ A1151.cpp
@@ -2,6 +2,7 @@
  * 1. 方法1 996ms, 先记录，再比较的方法太低效
  * 2. 方法2 100ms, 此方法比较高效，使用递归的方法直接找出LCA
  * 3. 写程序要严谨， 错一个字母就错来
+ * 4. 本题还可以通过先存储，再找路径的方法
  */
 
 #include <iostream>
@@ -228,4 +229,119 @@ int main() {
 }
 
 
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
 
+struct TreeNode {
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *parent;
+    TreeNode(int x) : value(x), left(NULL), right(NULL), parent(NULL) {}
+};
+map<int, TreeNode*> hashTable;
+vector<int> inorder;
+vector<int> preorder;
+
+TreeNode* buildTree(int inL, int inR, int preL, int preR) {
+    if (inL > inR) {
+        return NULL;
+    }
+    TreeNode *root = new TreeNode(preorder[preL]);
+    hashTable[root->value] = root;
+    for (int i = inL; i <= inR; i++) {
+        if (inorder[i] == root->value) {
+            int len = i - inL;
+            root->left = buildTree(inL, i - 1, preL + 1, preL + len);
+            if (root->left != NULL) {
+                root->left->parent = root;
+                hashTable[root->left->value] = root->left;
+            }
+            root->right = buildTree(i + 1, inR, preL + len + 1, preR);
+            if (root->right != NULL) {
+                root->right->parent = root;
+                hashTable[root->right->value] = root->right;
+            }
+        }
+    }
+    return root;
+}
+
+void findPath(int v, vector<int> & path) {
+    TreeNode *node = hashTable[v];
+    while (node != NULL) {
+        path.push_back(node->value);
+        node = node->parent;
+    }
+}
+
+int findAncestor(vector<int> & pathU, vector<int> & pathV) {
+    int p1 = pathU.size() - 1;
+    int p2 = pathV.size() - 1;
+    int result = -1;
+    while (p1 >= 0 && p2 >= 0) {
+        if (pathU[p1] == pathV[p2]) {
+            result =  pathU[p1];
+            p1--;
+            p2--;
+        } else {
+            break;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int M, N;
+    scanf("%d %d", &M, &N);
+    map<int, bool> mp;
+    inorder.resize(N);
+    preorder.resize(N);
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &inorder[i]);
+        mp[inorder[i]] = true;
+    }
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &preorder[i]);
+    }
+    TreeNode *root = buildTree(0, N - 1, 0, N -1);
+    
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        bool flag1, flag2;
+        int ancestor;
+        vector<int> pathU;
+        vector<int> pathV;
+        
+        scanf("%d %d", &u, &v);
+        flag1 = mp.find(u) == mp.end();
+        flag2 = mp.find(v) == mp.end();
+        
+        if (flag1 && flag2) {
+            printf("ERROR: %d and %d are not found.\n", u, v);
+        } else if (flag1) {
+            printf("ERROR: %d is not found.\n", u);
+        } else if (flag2) {
+            printf("ERROR: %d is not found.\n", v);
+        } else {
+            findPath(u, pathU);
+            findPath(v, pathV);
+            ancestor = findAncestor(pathU, pathV);
+            pathU.clear();
+            pathV.clear();
+            if (ancestor == u) {
+                printf("%d is an ancestor of %d.\n", u, v);
+            } else if (ancestor == v) {
+                printf("%d is an ancestor of %d.\n", v, u);
+            } else {
+                printf("LCA of %d and %d is %d.\n", u, v, ancestor);
+            }
+        }
+    }
+    
+    return 0;
+    
+}
