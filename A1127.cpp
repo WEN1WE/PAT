897462a022985881b464b84d8712a238e90bf445
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-04 09:51:04 +0800

    Update A1127.cpp

--- A1127.cpp
+++ A1127.cpp
@@ -1,6 +1,7 @@
 /*
  * 1. 层序遍历是，NULL不要加入队列中，只加入有效节点
  * 2. 使用两个队列，交替存储每一层
+ * 3. 本题也可以给节点加一个深度，来标示每一层
  */
 
 
@@ -99,6 +100,102 @@ int main() {
             printf(" ");
         }
         printf("%d", result[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector<int> inorder;
+vector<int> postorder;
+vector<int> zigzagging;
+
+struct TreeNode {
+    int value;
+    int level;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : value(x), level(-1), left(NULL), right(NULL) {}
+};
+
+TreeNode *buildTree(int inL, int inR, int postL, int postR) {
+    if (inL > inR) {
+        return NULL;
+    }
+    TreeNode *root = new TreeNode(postorder[postR]);
+    for (int i = inL; i<= inR; i++) {
+        if (inorder[i] == root->value) {
+            int len = i - inL;
+            root->left = buildTree(inL, i - 1, postL, postL + len - 1);
+            root->right = buildTree(i + 1, inR, postL + len, postR - 1);
+        }
+    }
+    return root;
+}
+
+vector<int> record[50];
+int deepest = -1;
+void traversal(TreeNode *root) {
+    root->level = 1;
+    queue<TreeNode *> q;
+    q.push(root);
+    
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        deepest = max(node->level, deepest);
+        record[node->level].push_back(node->value);
+        q.pop();
+        
+        if (node->left != NULL) {
+            node->left->level = node->level + 1;
+            q.push(node->left);
+        }
+        if (node->right != NULL) {
+            node->right->level = node->level + 1;
+            q.push(node->right);
+        }
+    }
+}
+
+void addTo(vector<int> & v) {
+    for (auto e : v) {
+        zigzagging.push_back(e);
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    inorder.resize(N);
+    postorder.resize(N);
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &inorder[i]);
+    }
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &postorder[i]);
+    }
+    TreeNode *root = buildTree(0, N - 1, 0, N - 1);
+    traversal(root);
+    
+    for (int i = 1; i <= deepest; i++) {
+        if (i % 2 != 0) {
+            reverse(record[i].begin(), record[i].end());
+        }
+        addTo(record[i]);
+    }
+    
+    for (int i = 0; i < N; i++) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", zigzagging[i]);
     }
     printf("\n");
     return 0;
