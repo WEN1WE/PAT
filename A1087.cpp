3eecbecfcf6084352babfe84b46d39c63590e173
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-06 10:16:21 +0800

    Update A1087.cpp

--- A1087.cpp
+++ A1087.cpp
@@ -4,8 +4,8 @@
  */
 
 #include <iostream>
-#include <map>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
@@ -14,13 +14,11 @@ const int MAX = 210;
 const int INF = 1000000000;
 int graph[MAX][MAX];
 int weight[MAX];
-int totalWeight[MAX];
+int happiness[MAX];
 int dist[MAX];
-vector<int> father[MAX];
-bool visited[MAX];
 int nOfPath[MAX];
-vector<int> path;
-vector<int> tempPath;
+vector<int> parent[MAX];
+bool visited[MAX];
 
 void init() {
     fill(graph[0], graph[0] + MAX * MAX, INF);
@@ -28,13 +26,12 @@ void init() {
 }
 
 int findMin() {
+    int minDist = INF;
     int v = -1;
-    int smallest = INF;
-    
-    for (int w = 0; w < N; w++) {
-        if (!visited[w] && dist[w] < smallest) {
-            smallest = dist[w];
-            v = w;
+    for (int i = 0; i < N; i++) {
+        if (!visited[i] && dist[i] < minDist) {
+            minDist = dist[i];
+            v = i;
         }
     }
     return v;
@@ -42,23 +39,23 @@ int findMin() {
 
 void relax(int v) {
     for (int w = 0; w < N; w++) {
-        if (!visited[w]) {
+        if (!visited[w] && graph[w][v] != INF) {
             if (dist[v] + graph[v][w] < dist[w]) {
                 dist[w] = dist[v] + graph[v][w];
                 nOfPath[w] = nOfPath[v];
                 
-                totalWeight[w] = weight[w] + totalWeight[v];
-                father[w].clear();
-                father[w].push_back(v);
+                parent[w].clear();
+                parent[w].push_back(v);
+                happiness[w] = happiness[v] + weight[w];
             } else if (dist[v] + graph[v][w] == dist[w]) {
                 nOfPath[w] += nOfPath[v];
                 
-                if (totalWeight[v] + weight[w] > totalWeight[w]) {
-                    totalWeight[w] = totalWeight[v] + weight[w];
-                    father[w].clear();
-                    father[w].push_back(v);
-                } else if (totalWeight[v] + weight[w] == totalWeight[w]) {
-                    father[w].push_back(v);
+                if (happiness[v] + weight[w] > happiness[w]) {
+                    happiness[w] = happiness[v] + weight[w];
+                    parent[w].clear();
+                    parent[w].push_back(v);
+                } else if (happiness[v] + weight[w] == happiness[w]) {
+                    parent[w].push_back(v);
                 }
             }
         }
@@ -68,7 +65,7 @@ void relax(int v) {
 void Dijkstra(int s) {
     nOfPath[s] = 1;
     dist[s] = 0;
-    totalWeight[s] = 0;
+    happiness[s] = 0;
     
     for (int i = 0; i < N; i++) {
         int v = findMin();
@@ -81,56 +78,63 @@ void Dijkstra(int s) {
 }
 
 int totalHappiness = 0;
-int maxHappiness = -1;
-void dfs(int v, int happiness, int count) {
+int maxAvg = -1;
+vector<int> path;
+vector<int> tempPath;
+void dfs(int v, int total, int cnt) {
     tempPath.push_back(v);
     if (v == 0) {
-        if (happiness / count > maxHappiness) {
-            totalHappiness = happiness;
+        int avg = total / cnt;
+        if (avg > maxAvg) {
+            maxAvg = avg;
+            totalHappiness = total;
             path = tempPath;
-            maxHappiness = happiness / count;
         }
-        tempPath.clear();
+        tempPath.pop_back();
         return;
     }
-    for (auto e : father[v]) {
-        dfs(e, weight[v] + happiness, ++count);
+    
+    for (auto e : parent[v]) {
+        dfs(e, weight[v] + total, cnt + 1);
     }
+    tempPath.pop_back();
 }
 
 int main() {
     init();
+    unordered_map<string, int> nameToID;
+    unordered_map<int, string> idToName;
     string start;
-    cin >> N >> K >> start;
-    map<string, int> nameToID;
-    map<int, string> idToName;
+    scanf("%d %d", &N, &K);
+    cin >> start;
     nameToID[start] = 0;
     idToName[0] = start;
     
     for (int i = 1; i < N; i++) {
         int h;
-        string name;
-        cin >> name >> h;
-        nameToID[name] = i;
-        idToName[i] = name;
+        string city;
+        cin >> city >> h;
+        nameToID[city] = i;
+        idToName[i] = city;
         weight[i] = h;
     }
     
     for (int i = 0; i < K; i++) {
+        string city1, city2;
         int cost, id1, id2;
-        string c1, c2;
-        cin >> c1 >> c2 >> cost;
-        id1 = nameToID[c1];
-        id2 = nameToID[c2];
+        cin >> city1 >> city2 >> cost;
+        id1 = nameToID[city1];
+        id2 = nameToID[city2];
         graph[id1][id2] = cost;
         graph[id2][id1] = cost;
     }
     
     int idROM = nameToID["ROM"];
+    Dijkstra(0);
     Dijkstra(nameToID[start]);
     dfs(idROM, 0, 0);
     reverse(path.begin(), path.end());
-    printf("%d %d %d %d\n", nOfPath[idROM], dist[idROM], totalHappiness, maxHappiness);
+    printf("%d %d %d %d\n", nOfPath[idROM], dist[idROM], totalHappiness, maxAvg);
     for (int i = 0; i < path.size(); i++) {
         if (i != 0) {
             printf("->");
