27a002a19bfd62db4eee72b643bf2aaf6a4bc911
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-06 12:00:48 +0800

    Update A1021.cpp

--- A1021.cpp
+++ A1021.cpp
@@ -12,76 +12,54 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <map>
 using namespace std;
 
-vector<int> father;
-vector<vector<int>> graph;
-set<int> result;
+const int MAX = 10010;
+vector<vector<int>> graph(MAX);
+int visited[MAX];
+int tempDeepest = -1;
+int deepest = -1;
 
-int root(int i) {
-    while (i != father[i]) {
-        father[i] = father[father[i]];
-        i = father[i];
+void dfs(int v, int deep) {
+    if (visited[v]) {
+        return;
     }
-    return i;
-}
-
-void Union(int p, int q) {
-    int i = root(p);
-    int j = root(q);
-    if (i != j) {
-        father[i] = j;
-    }
-}
-
-void init(int n) {
-    father.resize(n + 1);
-    for (int i = 1; i <= n; i++) {
-        father[i] = i;
-    }
-}
-
-int deepest = 0;
-int tempDeepest = 0;
-
-void dfs(int v, int preV, int deep) {
-    if (deep > tempDeepest) {
-        tempDeepest = deep;
-    }
+    tempDeepest = max(deep, tempDeepest);
+    visited[v] = true;
     for (auto e : graph[v]) {
-        if (e != preV) {
-            dfs(e, v, deep + 1);
-        }
+        dfs(e, deep + 1);
     }
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    init(n);
-    graph.resize(n + 1);
-    map<int, bool> mp;
+    int N;
+    scanf("%d", &N);
+    set<int> result;
     
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < N - 1; i++) {
         int node1, node2;
         scanf("%d %d", &node1, &node2);
         graph[node1].push_back(node2);
         graph[node2].push_back(node1);
-        Union(node1, node2);
     }
-    
-    for (int i = 1; i <= n; i++) {
-        mp[root(i)] = true;
+
+    int components = 0;
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            components++;
+            dfs(i, 1);
+        }
     }
-    
-    if (mp.size() != 1) {
-        printf("Error: %d components\n", mp.size());
+    if (components != 1) {
+        printf("Error: %d components", components);
         return 0;
     }
-    
-    for (int i = 1; i <= n; i++) {
-        dfs(i, 0, 1);
+
+    for (int i = 1; i <= N; i++) {
+        fill(visited, visited + MAX, false);
+        tempDeepest = -1;
+        dfs(i, 1);
+        
         if (tempDeepest > deepest) {
             deepest = tempDeepest;
             result.clear();
@@ -89,11 +67,10 @@ int main() {
         } else if (tempDeepest == deepest) {
             result.insert(i);
         }
-        tempDeepest = 0;
     }
     
     for (auto e : result) {
-        cout << e << endl;
+        printf("%d\n", e);
     }
     
     return 0;
