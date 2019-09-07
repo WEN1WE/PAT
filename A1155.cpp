eaf5a6228fe7398817c96f199bb65f97fc63eaf5
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-07 09:46:34 +0800

    Update A1155.cpp

--- A1155.cpp
+++ A1155.cpp
@@ -1,5 +1,6 @@
 /*
  * 1. 本题可以不用建树，直接dfs
+ * 2. 本题可以分开判断
  */
 
 #include <iostream>
@@ -68,4 +69,85 @@ int main() {
             printf("Min Heap\n");
         }
     }
+}
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+bool isMaxHeap;
+
+vector<int> levelorder;
+
+bool checkHeap(int root) {
+    int left = 2 * root;
+    int right = 2 * root + 1;
+    if (left > N) {
+        return true;
+    }
+    if (levelorder[root] > levelorder[left] != isMaxHeap) {
+        return false;
+    }
+    if (root * 2 + 1 <= N) {
+        if (levelorder[root] > levelorder[right] != isMaxHeap) {
+            return false;
+        }
+    }
+    return checkHeap(left) && checkHeap(right);
+}
+
+vector<int> path;
+void print() {
+    for (int i = 0; i < path.size(); i++) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", path[i]);
+    }
+    printf("\n");
+}
+
+void dfs(int root) {
+    if(root > N) {
+        return;
+    }
+    
+    path.push_back(levelorder[root]);
+    if (root * 2 > N) {
+        print();
+        path.pop_back();
+        return;
+    }
+    
+    dfs(root * 2 + 1);
+    dfs(root * 2);
+    path.pop_back();
+}
+
+
+
+int main() {
+    bool isHeap;
+    scanf("%d", &N);
+    levelorder.resize(N + 1);
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &levelorder[i]);
+    }
+    isMaxHeap = levelorder[1] > levelorder[2];
+    isHeap = checkHeap(1);
+    
+    dfs(1);
+    
+    if (isHeap) {
+        if (isMaxHeap) {
+            printf("Max Heap\n");
+        } else {
+            printf("Min Heap\n");
+        }
+    } else {
+        printf("Not Heap\n");
+    }
+
 }
