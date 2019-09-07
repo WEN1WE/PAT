1546e52967054e14321af5e661e08543ca13abdc
WEN1WE <35155819+WEN1WE@users.noreply.github.com>
2019-09-07 10:18:09 +0800

    Update A1154.cpp

--- A1154.cpp
+++ A1154.cpp
@@ -68,3 +68,54 @@ int main() {
     return 0;
 }
 
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Pair {
+	int end1;
+	int end2;
+};
+
+bool check(vector<Pair> & record, vector<int> & color) {
+	for (auto e : record) {
+		if (color[e.end1] == color[e.end2]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int N, M, K;
+	scanf("%d %d", &N, &M);
+	vector<Pair> record(M);
+
+
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &record[i].end1, &record[i].end2);
+	}
+
+	scanf("%d", &K);
+	for (int i = 0; i < K; i++) {
+		map<int, bool> mp;
+		vector<int> color(N);
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &color[j]);
+			mp[color[j]] = true;
+		}
+
+		if (check(record, color)) {
+			printf("%d-coloring\n", mp.size());
+		}
+		else {
+			printf("No\n");
+		}
+	}
+	system("pause");
+	return 0;
+
+}
+
+
